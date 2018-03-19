fb4500e05b6074b36055352c5b46def9e611c5e4
stacief510 <staciemfraser@gmail.com>
2018-03-18 22:45:53 -0700

    attempted to complete

--- scripts/app.js
+++ scripts/app.js
@@ -4,4 +4,32 @@ $(document).on('ready', function(){
 
   $('h1').text("Meet Mr. Fox"); // KEEP
 
-});
+  $('.first_name').text(data.first_name);
+
+  $('.occupation').text(data.occupation);
+
+  $('.location').text(data.location);
+
+
+// var rand = [];
+// for(var i = 0; i<=data.length; i++){
+// if (i === data.qualities){
+// 	rand.push(data.qualties[i]);
+// }
+// }
+
+// $('qualities').text(data.qualities[i]);
+
+//var rand = data[Math.floor(Math.random() * data.length)];
+
+
+
+// $('.firstSib').text(data.siblings[0].first_name).text(data.siblings[0].age);
+// $('.secondSib').text(data.siblings[1].first_name);
+// $('.thirdSib').text(data.siblings[2].first_name, data.siblings[2].age);
+// $('.fourthSib').text(data.siblings[2].first_name, data.siblings[2].age);
+//probably want us to use .child[1]to select sibling.
+
+$('img').attr("src", "data.profile_image");
+
+})
